433a4de10dbea1d047b629a7bc4a620703a124fd
sharada123 <sharadagadadhe111@gmail.com>
2026-04-24 12:42:44 +0530

    whitenoise removed

--- nageshwarcomputers/settings.py
+++ nageshwarcomputers/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'nageshwarcomputers.urls'
@@ -123,4 +123,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
