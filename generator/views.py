d0909792c1105e837a847372f796a02ce9ded0df
sharada123 <sharadagadadhe111@gmail.com>
2026-04-24 09:43:13 +0530

    ready project

--- generator/views.py
+++ generator/views.py
@@ -1,85 +1,101 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template.loader import render_to_string
-from weasyprint import HTML
-from .forms import UserForm
-from datetime import date
-#added for live site 
 from django.conf import settings
+from datetime import date
 import os
+
 from weasyprint import HTML, CSS
+from .forms import UserForm
 from .utils import get_fonts
 
-fonts = get_fonts()
 
-font_regular = fonts["regular"]
-font_bold = fonts["bold"]
-font_dejavu = fonts["dejavu"]
-def generate_pdf(request):
-    #font_path = os.path.join(settings.BASE_DIR, "fonts", "NotoSansDevanagari-Regular.ttf")
-    if request.method == 'GET':
-        form = UserForm()
-        return render(request, 'form.html', {'form': form})
+# ---------- Helpers ----------
+def file_url(path):
+    return "file://" + path.replace("\\", "/")
 
-    elif request.method == 'POST':
-        form = UserForm(request.POST)
-        
-        if form.is_valid():
-            data = form.cleaned_data
-            # 🔥 Convert to Marathi digits
-            data['mobile'] = to_marathi_number(data.get('mobile', ''))
-            data['age'] = to_marathi_number(data.get('age', ''))
-            
-            today = date.today()
 
-            # 🔥 Format: DD/MM/YYYY
-            formatted_date = today.strftime("%d/%m/%Y")
-
-            # 🔥 Convert to Marathi digits
-            data['today'] = to_marathi_number(formatted_date)
-
-            html = render_to_string('pdf_template.html', data)
-
-            response = HttpResponse(content_type='application/pdf')
-            response['Content-Disposition'] = 'attachment; filename="output.pdf"'
-
-            #HTML(string=html).write_pdf(response)
-            #added for live
-            HTML(string=html).write_pdf(
-            response,
-            stylesheets=[
-            CSS(string=f"""
-            @font-face {{
-                font-family: 'NotoDev';
-                src: url('file://{font_regular}');
-            }}
-
-            @font-face {{
-                font-family: 'NotoDev';
-                src: url('file://{font_bold}');
-                font-weight: bold;
-            }}
-
-            @font-face {{
-                font-family: 'DejaVu';
-                src: url('file://{font_dejavu}');
-            }}
-
-            body {{
-                font-family: 'NotoDev', 'DejaVu', sans-serif;
-            }}
-
-            b, strong {{
-                font-family: 'NotoDev';
-                font-weight: bold;
-            }}
-        """)
-    ]
-)
-            return response
-
-        return render(request, 'form.html', {'form': form})
 def to_marathi_number(number):
     eng = "0123456789"
     mar = "०१२३४५६७८९"
     return str(number).translate(str.maketrans(eng, mar))
+
+
+# ---------- Fonts ----------
+fonts = get_fonts()
+
+font_regular = file_url(fonts["regular"])
+font_bold = file_url(fonts["bold"])
+font_dejavu = file_url(fonts["dejavu"])
+
+
+def generate_pdf(request):
+
+    # logo path (SAFE)
+    logo_path = os.path.abspath(os.path.join(settings.BASE_DIR, 'static', 'logo', 'logo.png'))
+    print(logo_path)
+    if request.method == 'GET':
+        form = UserForm()
+        return render(request, 'form.html', {'form': form})
+
+    form = UserForm(request.POST)
+
+    if form.is_valid():
+        data = form.cleaned_data
+
+        # Marathi numbers
+        data['mobile'] = to_marathi_number(data.get('mobile', ''))
+        data['age'] = to_marathi_number(data.get('age', ''))
+
+        # date
+        today = date.today().strftime("%d/%m/%Y")
+        data['today'] = to_marathi_number(today)
+
+        # assets
+        data['logo_path'] = logo_path
+
+        # render HTML
+        html_string = render_to_string('pdf_template.html', data)
+
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename="output.pdf"'
+
+        HTML(string=html_string).write_pdf(
+            response,
+            stylesheets=[
+                CSS(string=f"""
+                @font-face {{
+                    font-family: 'Marathi';
+                    src: url('{font_regular}');
+                    font-weight: normal;
+                }}
+
+                @font-face {{
+                    font-family: 'Marathi';
+                    src: url('{font_bold}');
+                    font-weight: bold;
+                }}
+
+                @font-face {{
+                    font-family: 'NumberFont';
+                    src: url('{font_dejavu}');
+                }}
+
+                body {{
+                    font-family: 'Marathi', sans-serif;
+                }}
+
+                .num {{
+                    font-family: 'NumberFont';
+                }}
+
+                b, strong {{
+                    font-weight: bold;
+                }}
+                """)
+            ]
+        )
+
+        return response
+
+    return render(request, 'form.html', {'form': form})
